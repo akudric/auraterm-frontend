a86c34acf856392b9ea5484b77b4d7fdcd0c54e2
antonio.kudric <antonio4.kudric@gmail.com>
2025-08-12 16:16:42 +0200

    Added api.js

---
+++ src/api.js
@@ -0,0 +1,3 @@
+const STRAPI_BASE_URL = 'https://ethical-champion-a6f0cd3e63.strapiapp.com';
+const STAPI_OUR_REVIEWS_API_CALL = "/api/our-reviews?populate=avatar"
+const STRAPI_PRICES_API_CALL = "/api/pricing-options"
